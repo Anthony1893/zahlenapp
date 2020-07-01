b820ff051ea8bbeb91d8025bca27dd7cbb79e5ab
admin <Anthony.wurmser@gmail.com>
2020-07-01 12:16:10 +0200

    SOunds und viele änderungen

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,17 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Xiao Xiao ' ),
+      home: MyHomePage(title: 'A long time ago in a galaxy far, far away' ),
 
     );
   }
+
 }
 
 class MyHomePage extends StatefulWidget {
@@ -59,44 +60,60 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
-
-      playLocalAsset();
-
-      // Do stuff on phone shake
+      print('should play');
+      playShakeSound();
     });
     // To close: detector.stopListening();
     // ShakeDetector.waitForStart() waits for user to call detector.startListening();
   }
-  Future<AudioPlayer> playLocalAsset() async {
+  Future<AudioPlayer> playLocalAsset(String name) async {
     AudioCache cache = new AudioCache();
     //At the next line, DO NOT pass the entire reference such as assets/yes.mp3. This will not work.
     //Just pass the file name only.
-    return await cache.play("moan.mp3");
+    return await cache.play(name);
 
   }
 
-  int _counter = 0;
+  playShakeSound() {
+    playLocalAsset(_songs[_counter % _songs.length]);
+  }
 
-  void _incrementCounter() {
+  int _counter = 1;
+  final _songs = ["sound5.mp3", "moan.mp3", "sound1.mp3", "sound2.mp3","sound3.mp3"];
+
+   void _incrementCounter() {
     setState(() {
-
+      if (_counter > 4) {
+        _counter = 1;
+      }
+      else
+    {
+      _counter++;
+    }
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+
     });
   }
 
   void _increment() {
     setState(() {
+      if(_counter < 2){
+        _counter = 5;
+      }
+      else
+      {
+        _counter--;
+      }
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter--;
+
     });
   }
 
@@ -110,23 +127,26 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+
         appBar: AppBar(
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
+
           title: Text(widget.title),
+
         ),
 
 
-        backgroundColor: Colors.white70,
+        backgroundColor: Colors.black,
 
         body: Center(
-
 
 
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
 
           child: Column(
+
 
             // Column is also a layout widget. It takes a list of children and
             // arranges them vertically. By default, it sizes itself to fit its
@@ -145,17 +165,24 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
+            'Shake your Phone and choose between 5 sounds',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.yellow, fontSize: 18.0),
+          ),
+              Image.asset('assets/fort.png'),
+             // Text(
 
-                'Zähle wärend du Xiao Xiao hörst',
-            style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text(
-                    'Schüttel dein Handy',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
+            //    'Zähle wärend du Xiao Xiao hörst',
+       //     style: TextStyle(fontWeight: FontWeight.bold),
+        //      ),
+        //      Text(
+         //           'Schüttel dein Handy',
+         //     ),
+          Text(
+             '$_counter' ,
+             style: TextStyle(fontWeight: FontWeight.bold, color: Colors.yellow, fontSize: 50.00),
+
+
+            ),
             ],
           ),
 
@@ -170,7 +197,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 tooltip: 'Increment',
 
                 child: Icon(Icons.remove),
-                backgroundColor: Colors.green,
+
               ),
               Container(
                 width: 250.0,
@@ -182,6 +209,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: _incrementCounter,
                 tooltip: 'Increment',
                 child: Icon(Icons.add), // This trailing comma makes auto-formatting nicer for build methods
+
               )
             ]
         )
